5e2861f30b39ab0c2e7b80e76dfae09efb228bbb
Lonewoelf <38475792+Lonewoelf@users.noreply.github.com>
2020-05-01 14:46:07 +0200

    Cleaned main

--- WaterScape2/WaterScape.h
+++ WaterScape2/WaterScape.h
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <GL/glew.h>
+#include <GL/glut.h>
 
 class WaterScape
 {
+public:
+	WaterScape();
+	~WaterScape();
+	void init(int argc, char** argv);
+	static void display();
 };
 
