a165e96f2773769f9df98c7b2cb15729ea79b2db
Lonewoelf <38475792+Lonewoelf@users.noreply.github.com>
2020-06-02 14:26:11 +0200

    cubeScape :p

--- WaterScape2/WaterScape.cpp
+++ WaterScape2/WaterScape.cpp
@@ -4,8 +4,9 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 #include <windows.h>
+#include <math.h>
 
-GLfloat angle = 0.0;
+float xpos = 0, ypos = 0, zpos = 0, xrot = 0, yrot = 0, angle = 0.0;
 
 GLuint texture;
 
@@ -14,6 +15,9 @@ char keyStates[256];
 void cub2();
 
 GLuint cubelist;
+
+float positionz[10];
+float positionx[10];
 
 
 WaterScape::WaterScape()
@@ -25,47 +29,56 @@ WaterScape::~WaterScape()
 	
 }
 
+void cubePositions(void) { //set the positions of the cubes
+
+	for (int i = 0; i < 10; i++)
+	{
+		positionz[i] = rand() % 5 + 5;
+		positionx[i] = rand() % 5 + 5;
+	}
+}
+
 void cube() {
 	cubelist = glGenLists(1);
 	glNewList(cubelist, GL_COMPILE);
-	glPushMatrix();
-	glTranslatef(1, 0, 0);
-	glRotatef(angle, 1.0, 0.0, 0.0);
-	glRotatef(angle, 0.0, 1.0, 0.0);
-	glRotatef(angle, 0.0, 0.0, 1.0);
-	glColor3f(0.0, 1.0, 0.0);
-	glutSolidCube(1);
-	glPopMatrix();
 
-	//glPushMatrix();
-	//glTranslatef(0, 1, 0);
-	//glRotatef(angle, 1.0, 0.0, 0.0);
-	//glRotatef(angle, 0.0, 1.0, 0.0);
-	//glRotatef(angle, 0.0, 0.0, 1.0);
-	//glColor3f(0.0, 1.0, 0.0);
-	//glutSolidCube(1);
-	//glPopMatrix();
+	for (int i = 0; i < 10; i++){
+		glPushMatrix();
+		glTranslated(-positionx[i + 1] * 10, 0, -positionz[i + 1] * 10); 
+		glRotatef(angle, 1.0, 0.0, 0.0);
+		glRotatef(angle, 0.0, 1.0, 0.0);
+		glRotatef(angle, 0.0, 0.0, 1.0);
+		glPushMatrix();
 
+		glColor3f(0.0, 1.0, 0.0);
+		glutSolidCube(1);
+		glPopMatrix();
+	}
 	glEndList();
+}
+
+void camera(void) {
+	glRotatef(xrot, 1.0, 0.0, 0.0);
+	glRotatef(yrot, 0.0, 1.0, 0.0);
+	glTranslated(-xpos, -ypos, -zpos);
 }
 
 void WaterScape::init(int argc, char** argv)
 {
 	glutInit (&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-	glutGameModeString("1920×1080:32@75");
-	glutEnterGameMode();
+	cubePositions();
+	//glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+	//glutGameModeString("1920×1080:32@75");
+	//glutEnterGameMode();
     glutInitDisplayMode (GLUT_DOUBLE);
-    //glutInitWindowSize (SCREEN_WIDTH, SCREEN_HEIGHT);
-    //glutInitWindowPosition (SCREEN_POS_X, SCREEN_POS_Y);
-    //glutCreateWindow ("WaterScape 2");
+    glutInitWindowSize (SCREEN_WIDTH, SCREEN_HEIGHT);
+    glutInitWindowPosition (SCREEN_POS_X, SCREEN_POS_Y);
+    glutCreateWindow ("WaterScape 2");
     glutDisplayFunc (display);
     glutIdleFunc (display);
     glutReshapeFunc (reshape);
 	glutKeyboardFunc(keyPressed);
-
 	cube();
-
     glutMainLoop ();
 }
 
@@ -97,6 +110,8 @@ void WaterScape::display()
 	glEnable(GL_TEXTURE_GEN_S);
 	glEnable(GL_TEXTURE_GEN_T);
 	glDeleteTextures(1, &texture);
+	glLoadIdentity();
+	camera();
 	glCallList(cubelist);
 	glutSwapBuffers();
 	updateFps();
@@ -113,33 +128,49 @@ void WaterScape::reshape(int width, int height) {
 
 void WaterScape::keyPressed(unsigned char key, int x, int y)
 {
-/*	if (key == 'r') {
-		dlr = 1.0; //change light to red
-		dlg = 0.0;
-		dlb = 0.0;
-	}
-	if (key == 'g') {
-		dlr = 0.0; //change light to green
-		dlg = 1.0;
-		dlb = 0.0;
-	}
-	if (key == 'b') {
-		dlr = 0.0; //change light to blue
-		dlg = 0.0;
-		dlb = 1.0;
-	}
-	if (key == 'w') {
-		ly += 10.0; //move the light up
-	}
-	if (key == 's') {
-		ly -= 10.0; //move the light down
-	}
-	if (key == 'a') {
-		lx -= 10.0; //move the light left
-	}
-	if (key == 'd') {
-		lx += 10.0; //move the light right
-	}*/
+    if (key=='x')
+    {
+    xrot += 1;
+    if (xrot >360) xrot -= 360;
+    }
+
+    if (key=='z')
+    {
+    xrot -= 1;
+    if (xrot < -360) xrot += 360;
+    }
+
+    if (key=='w')
+    {
+    float xrotrad, yrotrad;
+    yrotrad = (yrot / 180 * 3.141592654f);
+    xrotrad = (xrot / 180 * 3.141592654f);
+    xpos += float(sin(yrotrad)) ;
+    zpos -= float(cos(yrotrad)) ;
+    ypos -= float(sin(xrotrad)) ;
+    }
+
+    if (key=='s')
+    {
+    float xrotrad, yrotrad;
+    yrotrad = (yrot / 180 * 3.141592654f);
+    xrotrad = (xrot / 180 * 3.141592654f);
+    xpos -= float(sin(yrotrad));
+    zpos += float(cos(yrotrad)) ;
+    ypos += float(sin(xrotrad));
+    }
+
+    if (key=='d')
+    {
+    yrot += 1;
+    if (yrot >360) yrot -= 360;
+    }
+
+    if (key=='a')
+    {
+    yrot -= 1;
+    if (yrot < -360)yrot += 360;
+    }
 
 	if (key == 'q')
 	{
@@ -178,6 +209,7 @@ GLuint WaterScape::LoadTexture(const char* filename, int width, int height)
 
 	gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
 	free(data);
+
 	return texture;
 }
 
