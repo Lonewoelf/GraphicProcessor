4882e2157e9c01cd2306ee7c300e90c301de4ad8
Lonewoelf <38475792+Lonewoelf@users.noreply.github.com>
2020-06-07 21:01:02 +0200

    First RAW render

--- WaterScape2/HeightField.h
+++ WaterScape2/HeightField.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <string>
+#include <windows.h>
+
+#define MAP_SIZE_X 1024
+#define MAP_SIZE_Y 1024
+
 class HeightField
 {
 public:
@@ -7,5 +12,10 @@ public:
 	virtual ~HeightField();
 	bool Create(std::string fileName, int hWidth, int hHeight);
 	void Render(void);
+
+private:
+	BYTE hHeightField[1024][1024];
+	int hmHeight;
+	int hmWidth;
 };
 
